f2df69a72823ec748b3989b7066cab7687bb2bfb
Koji Tanaka <kj.tanaka@gmail.com>
2014-05-23 12:12:25 -0400

    minor fix, and 0.1.0 is good to go

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,24 +26,24 @@ munge_key = secrets['munge_key']
 packages = %w{munge}
 
 packages.each do |pkg|
-	package pkg do
-		action :install
-	end
+  package pkg do
+    action :install
+  end
 end
 
 execute "create_munge_key" do
-	command "echo #{munge_key} > /etc/munge/munge.key"
-	creates "/etc/munge/munge.key"
+  command "echo #{munge_key} > /etc/munge/munge.key"
+  creates "/etc/munge/munge.key"
 end
 
 file "/etc/munge/munge.key" do
-	mode "0400"
-	user "munge"
-	group "munge"
-	notifies :restart, "service[munge]", :immediately
+  mode "0400"
+  user "munge"
+  group "munge"
+  notifies :restart, "service[munge]", :immediately
 end
 
 service "munge" do
-	supports :restart => true
-	action [:enable, :start]
+  supports :restart => true
+  action [:enable, :start]
 end
