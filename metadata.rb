8dcb0e3c8e9bb9c1d81a1d50b2dc3565463ed153
Koji Tanaka <kj.tanaka@gmail.com>
2014-05-23 15:47:59 -0400

    adding support os on metadata

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,8 @@ description      'Installs/Configures munge'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w(redhat centos fedora).each do |os|
+	  supports os
+end
+
 depends 'yum-epel'
